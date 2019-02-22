dc40607ba718116f08ae4be6b154759174679f28
Joshua Gitter <joshuacod4@yahoo.com>
2019-02-21 22:02:56 -0500

    refactored the code into separate functions

--- plot.py
+++ plot.py
@@ -20,102 +20,102 @@ import plotly.graph_objs as go
 
 # Create random data with numpy
 import numpy as np
-SIZE_OF_GRAPH = 20  #these numbers must be divisible with a remainder of 0
-MAX_AXIS_VALUE = int(SIZE_OF_GRAPH/2)
-BOX_SIZE = 5
-NUM_OF_BOXES = int((SIZE_OF_GRAPH/BOX_SIZE)*2)
-NUM_OF_CHANNELS = [0] * 32
+def main():
+    SIZE_OF_GRAPH = 20  #these numbers must be divisible with a remainder of 0
+    MAX_AXIS_VALUE = int(SIZE_OF_GRAPH/2)
+    BOX_SIZE = 5
+    NUM_OF_BOXES = int((SIZE_OF_GRAPH/BOX_SIZE)*2)
+    NUM_OF_CHANNELS = [0] * 32
 
-#Max size of graph if size=10 is 20x20 because the walk can put you at 20
-#if no walk it is 10x10
-random_x = []
-random_y = []
-box_array = []
-# max_x = SIZE_OF_GRAPH
-# max_y = SIZE_OF_GRAPH
-count=0
-box_name = 'box'
-x1 = -SIZE_OF_GRAPH
-y1 = -SIZE_OF_GRAPH
-x2 = x1+BOX_SIZE
-y2 = y1+BOX_SIZE
-
-#while max_x<0 and max_y<0: # change this to while they are both greater then -max and less than max
-    #box_name+=str(count)
-    #box_array[count]
-#while x2!=SIZE_OF_GRAPH and y2!=0:
-for i in range(0,NUM_OF_BOXES):
+    #Max size of graph if size=10 is 20x20 because the walk can put you at 20
+    #if no walk it is 10x10
+    random_x = []
+    random_y = []
+    box_array = []
+    # max_x = SIZE_OF_GRAPH
+    # max_y = SIZE_OF_GRAPH
+    box_name = 'box'
     x1 = -SIZE_OF_GRAPH
+    y1 = -SIZE_OF_GRAPH
     x2 = x1+BOX_SIZE
-    for p in range(0,NUM_OF_BOXES):
-        box_array.append([x1,-y1,
-                            x2,-y1,
-                            x1,-y2,
-                            x2,-y2]) #probably need 4 sets of coordinates in here 
-        
-        #print(box_array)
-        x1+=BOX_SIZE
-        x2+=BOX_SIZE
-        #count+=1
+    y2 = y1+BOX_SIZE
 
-    y1+=BOX_SIZE
-    y2+=BOX_SIZE
+    createBoxes(x1,x2,y1,y2,box_array,SIZE_OF_GRAPH,BOX_SIZE,NUM_OF_BOXES)
+    createGraph1(random_x,random_y,MAX_AXIS_VALUE,box_array)
+    graphWalk(random_x,random_y,MAX_AXIS_VALUE)
+    # for x in range(0,len(box_array)):
+    #     print(box_array[x])
 
-
-
-for x in range(0,len(box_array)):
-    print(box_array[x])
-
-
-
-N = int(input("How many nodes would you like to run this simulation with? "))
-#print("Nodes: ",N)
-for i in range (0,N):
-    random_x.append(np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE))
-    random_y.append(np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE))
-print(random_x)
-print(random_y)
-# Create a trace
-trace = go.Scatter(
-    x = random_x,
-    y = random_y,
-    mode = 'markers'
-)
+def createBoxes(x1,x2,y1,y2,box_array,SIZE_OF_GRAPH,BOX_SIZE,NUM_OF_BOXES):
+    for i in range(0,NUM_OF_BOXES):
+        x1 = -SIZE_OF_GRAPH
+        x2 = x1+BOX_SIZE
+        for p in range(0,NUM_OF_BOXES):
+            box_array.append([x1,-y1,
+                                x2,-y1,
+                                x1,-y2,
+                                x2,-y2])
+            x1+=BOX_SIZE
+            x2+=BOX_SIZE
+        y1+=BOX_SIZE
+        y2+=BOX_SIZE
 
 
-#first trace
-data = [trace]
-#this makes the file name random so that the file isnt overwritten 
-#each time this program runs
-randFileName = np.random.randn()
-nameOfFile = "scatter"+ str(randFileName)
+def createGraph1(random_x,random_y,MAX_AXIS_VALUE,box_array):
+    import numpy as np
+    N = int(input("How many nodes would you like to run this simulation with? "))
+    #print("Nodes: ",N)
+    for i in range (0,N):
+        random_x.append(np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE))
+        random_y.append(np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE))
+        for j in range(0,len(box_array)):
+            if(random_x[i]>=box_array[j][0] and random_x[i]<=box_array[j][2] and random_y[i]<=box_array[j][1] and random_y[i]>=box_array[j][5]):
+                print(j)
+                print(box_array[j])
+    print(random_x)
+    print(random_y)
+    # Create a trace
+    trace = go.Scatter(
+        x = random_x,
+        y = random_y,
+        mode = 'markers'
+    )
+    #first trace
+    data = [trace]
+    #this makes the file name random so that the file isnt overwritten 
+    #each time this program runs
+    randFileName = np.random.randn()
+    nameOfFile = "scatter"+ str(randFileName)
 
-# Plot and embed in ipython notebook!
-py.plot(data, filename=nameOfFile)
+    # Plot and embed in ipython notebook!
+    py.plot(data, filename=nameOfFile)
 
-# or plot with: plot_url = py.plot(data, filename='basic-line')
+    # or plot with: plot_url = py.plot(data, filename='basic-line')
 
-#modify the x and y values "the random walk"
-for x in range(0,len(random_x)):
-    movex = np.random.uniform(-SIZE_OF_GRAPH, SIZE_OF_GRAPH)
-    movey = np.random.uniform(-SIZE_OF_GRAPH, SIZE_OF_GRAPH)
-    random_x[x] += movex
-    random_y[x] += movey
+def graphWalk(random_x,random_y, MAX_AXIS_VALUE):
+    #Modify the x and y value
+    for x in range(0,len(random_x)):
+        movex = np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE)
+        movey = np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE)
+        random_x[x] += movex
+        random_y[x] += movey
 
-#second trace
-trace2 = go.Scatter(
-    x = random_x,
-    y = random_y,
-    mode = 'markers'
-)
+    #second trace
+    trace2 = go.Scatter(
+        x = random_x,
+        y = random_y,
+        mode = 'markers'
+    )
 
 
-#first trace
-data2 = [trace2]
-#this makes the file name random so that the file isnt overwritten 
-#each time this program runs
-randFileName2 = np.random.randn()
-nameOfFile2 = "scatterWALK"+ str(randFileName2)
+    #first trace
+    data2 = [trace2]
+    #this makes the file name random so that the file isnt overwritten 
+    #each time this program runs
+    randFileName2 = np.random.randn()
+    nameOfFile2 = "scatterWALK"+ str(randFileName2)
 
-# Plot and embed in ipython notebook!
-py.plot(data2, filename=nameOfFile2)
+    # Plot and embed in ipython notebook!
+    py.plot(data2, filename=nameOfFile2)
+
+main()
--- test.py
+++ test.py
@@ -67,54 +67,99 @@
 
 import numpy as np
 SIZE_OF_GRAPH = 20  #these numbers must be divisible with a remainder of 0
+MAX_AXIS_VALUE = 20#int(SIZE_OF_GRAPH/2)
 BOX_SIZE = 5
 NUM_OF_BOXES = int((SIZE_OF_GRAPH/BOX_SIZE)*2)
-NUM_OF_CHANNELS = [0] * 32
+NUM_OF_CHANNELS = 32
 
 #Max size of graph if size=10 is 20x20 because the walk can put you at 20
 #if no walk it is 10x10
 random_x = []
 random_y = []
 box_array = []
+channel_array = [0] * NUM_OF_CHANNELS
 # max_x = SIZE_OF_GRAPH
 # max_y = SIZE_OF_GRAPH
-#count=0
-box_name = 'box'
+# count=0
+# box_name = 'box'
 x1 = -SIZE_OF_GRAPH
 y1 = -SIZE_OF_GRAPH
 x2 = x1+BOX_SIZE
 y2 = y1+BOX_SIZE
+
+
+# array = [[0,1,2,3],[3,2,4],[2,3]]
+# print(array)
+
+
+w, h = 32, 64;
+Matrix = [[0 for x in range(w)] for y in range(h)] 
+#print(Matrix)
+
+for i in range (0,64):
+	for j in range (0,32):
+		Matrix[i][j] = np.random.randint(0,4)
+
+
+
+# for i in range(0,50):
+# 	print (np.random.randint(0,4))
+
+print (Matrix)
+
+
+# for i in range(0,NUM_OF_CHANNELS):
+
 
 #while max_x<0 and max_y<0: # change this to while they are both greater then -max and less than max
     #box_name+=str(count)
     #box_array[count]
 #while x2!=SIZE_OF_GRAPH and y2!=0:
 for i in range(0,NUM_OF_BOXES):
-	x1 = -SIZE_OF_GRAPH
-	x2 = x1+BOX_SIZE
-	for p in range(0,NUM_OF_BOXES):
-		box_array.append([x1,-y1,
-							x2,-y1,
-		                    x1,-y2,
-		                    x2,-y2]) #probably need 4 sets of coordinates in here 
-		
-		#print(box_array)
-		x1+=BOX_SIZE
-		x2+=BOX_SIZE
-		#count+=1
+    x1 = -SIZE_OF_GRAPH
+    x2 = x1+BOX_SIZE
+    for p in range(0,NUM_OF_BOXES):
+        box_array.append([x1,-y1,
+                            x2,-y1,
+                            x1,-y2,
+                            x2,-y2]) #probably need 4 sets of coordinates in here 
+        
+        #print(box_array)
+        x1+=BOX_SIZE
+        x2+=BOX_SIZE
+        #count+=1
 
-	y1+=BOX_SIZE
-	y2+=BOX_SIZE
-
-
-
-for x in range(0,len(box_array)):
-	print(box_array[x])
-
-
-print(NUM_OF_CHANNELS)
-    
+    y1+=BOX_SIZE
+    y2+=BOX_SIZE
 
 
 
 
+
+# for x in range(0,len(box_array)):
+# 	print(box_array[x])
+
+
+# print(NUM_OF_CHANNELS)
+
+# for i in range (0,1):
+#     random_x.append(np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE))
+#     random_y.append(np.random.uniform(-MAX_AXIS_VALUE, MAX_AXIS_VALUE))
+#     for j in range(0,len(box_array)):
+#         if(random_x[i]>=box_array[j][0] and random_x[i]<=box_array[j][2] and random_y[i]<=box_array[j][1] and random_y[i]>=box_array[j][5]):
+# 	        print(j)
+#         	print(box_array[j])
+# 	        print(random_x[i])
+# 	        print(random_y[i])
+# 	        print(box_array[j][0])
+# 	        print(box_array[j][2])
+# 	        print(box_array[j][1])
+# 	        print(box_array[j][5])
+# 	        print("\n")
+
+# print(len(box_array))
+
+# print(random_x[0])
+# print(random_y[0])
+# print(random_x)
+# print(random_y)
